c59d542e376664df522b15573b0fb1207607194a
BluDood <bludood@bludood.com>
2025-11-17 11:23:57 +0100

    feat: add links to dashboard home

--- web/src/routes/dashboard/+page.svelte
+++ web/src/routes/dashboard/+page.svelte
@@ -5,6 +5,28 @@
 {#if $userStore}
   <main>
     <h1>Welcome, {$userStore?.username}!</h1>
+    <div class="links">
+      <a href="/dashboard/files">
+        <span class="material-icons"> folder_open </span>
+        Files
+      </a>
+      <a href="/dashboard/pastes">
+        <span class="material-icons"> content_paste </span>
+        Pastes
+      </a>
+      <a href="/dashboard/collections">
+        <span class="material-icons"> collections </span>
+        Collections
+      </a>
+      <a href="/dashboard/sharing">
+        <span class="material-icons"> share </span>
+        Sharing
+      </a>
+      <a href="/dashboard/usage">
+        <span class="material-icons"> bar_chart </span>
+        Usage
+      </a>
+    </div>
   </main>
 {/if}
 
@@ -14,5 +36,33 @@
     flex-direction: column;
     gap: 10px;
     animation: appear 500ms ease;
+  }
+
+  .links {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  .links a {
+    all: unset;
+    cursor: pointer;
+    padding: 5px 10px;
+    border-radius: 5px;
+    background: #222;
+    transition: 200ms ease;
+    outline: 1px solid transparent;
+    outline-offset: 2px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+
+  .links a:hover {
+    opacity: 0.8;
+  }
+
+  .links a:focus {
+    outline-color: #555;
   }
 </style>
